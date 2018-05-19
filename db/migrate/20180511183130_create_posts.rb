d0e62c3ab04662204b2123aa9d80579fbde663a6
Dinah Shi <dinahshi28@gmail.com>
2018-05-19 16:32:25 -0400

    rename post content attribute to body

--- db/migrate/20180511183130_create_posts.rb
+++ db/migrate/20180511183130_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :name
       t.string :title
-      t.text :content
+      t.text :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2018_05_11_185334) do
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.string "title"
-    t.text "content"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
